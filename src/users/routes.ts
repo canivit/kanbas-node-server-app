397629227a84b346181e4022bb7ce535b6d46757
Can Ivit <canivit@users.noreply.github.com>
2023-11-27 13:08:22 -0500

    implement get account

--- src/users/routes.ts
+++ src/users/routes.ts
@@ -1,19 +1,31 @@
 import { Express, Request } from "express";
 import * as dao from "./dao";
+import { User } from "./schema";
+
+let currentUser: User | false = false;
 
 export function userRoutes(app: Express) {
   app.post(
     "/api/users/signin",
     async (req: Request<{}, {}, Credentials>, res) => {
       const { username, password } = req.body;
-      const user = await dao.findUserByCredentials(username, password);
-      if (!user) {
+      currentUser =
+        (await dao.findUserByCredentials(username, password)) ?? false;
+      if (!currentUser) {
         res.status(401).send("Invalid credentials");
         return;
       }
-      res.send(user);
+      res.send(currentUser);
     }
   );
+
+  app.get("/api/users/account", async (_req, res) => {
+    if (!currentUser) {
+      res.status(401).send("Not signed in");
+      return;
+    }
+    res.send(currentUser);
+  });
 }
 
 type Credentials = {
